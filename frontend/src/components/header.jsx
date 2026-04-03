e5165da2a847b2c38f8a1643d66f1eb45eeb268f
Anurag-Karthik <anuragpolamarasetty@gmail.com>
2026-04-03 11:11:47 +0530

    feat: Change Header Component Rendering from Global level to App level

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,9 +1,22 @@
-import logo from '/src/assets/images/lucidus-ai-logo.png'
+import { useLocation, useNavigate } from 'react-router-dom';
 
-export default function Header() {
+import logo from '/src/assets/images/lucidus-ai-logo.png'
+    
+export default function Header({ handleBackNavInAssessment }) {
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
+
+    const homeNavigationBtnHandler = () => {
+        if (pathname !== "/" && pathname !== "/assessment") {
+            navigate("/");
+        } else if (pathname === "/assessment") {
+            handleBackNavInAssessment();
+        }
+    };
+
     return (
         <div className="w-full h-15 px-6 pt-[17px] pb-[11px] mb-5 text-left bg-primary">
-            <img src={logo} className='h-8 w-7' alt="Lucidus AI Logo" />
+            <img src={logo} onClick={homeNavigationBtnHandler} className='h-8 w-7' alt="Lucidus AI Logo" />
         </div>
     );
 }
--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,8 +1,9 @@
 import { useNavigate } from "react-router-dom";
 
 import Button from "../components/button";
+import Header from "../components/header";
 
-import arrow from "../assets/images/arrow_forward.svg";
+import arrow from "../assets/images/icons/arrow_forward.svg";
 
 export default function Landing() {
     const navigate = useNavigate();
@@ -12,19 +13,22 @@ export default function Landing() {
     }
 
     return (
-        <div className="text-center">
-            <div className="mt-25 text-xl/relaxed text-white font-bold">
-                <div>Ready To Discover Your</div>
-                <div>Six Affinities?<sup className="text-xs">TM</sup></div>
+        <>
+            <Header />
+            <div className="text-center">
+                <div className="mt-25 text-xl text-white font-bold">
+                    <div>Ready To Discover Your</div>
+                    <div>Six Affinities?<sup className="text-xs">TM</sup></div>
+                </div>
+                <div className="mt-109">
+                    <Button onClick={startQuestBtnHandler} className='rounded-4xl font-bold text-sm' type="olive-green">
+                        <div className="flex items-center ml-2">
+                            Start Quest
+                            <img src={arrow} className="ml-2" alt="Arrow" />
+                        </div>
+                    </Button>
+                </div>
             </div>
-            <div className="mt-109">
-                <Button onClick={startQuestBtnHandler} className='rounded-4xl font-bold text-sm' type="olive-green">
-                    <div className="flex items-center">
-                        Start Quest
-                        <img src={arrow} className="ml-2" alt="Arrow" />
-                    </div>
-                </Button>
-            </div>
-        </div>
+        </>
     )
 }
