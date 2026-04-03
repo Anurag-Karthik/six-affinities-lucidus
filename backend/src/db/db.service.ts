4f7133bb3b5cc206a8f9a25f9f909c599857f356
Anurag-Karthik <anuragpolamarasetty@gmail.com>
2026-04-03 12:37:17 +0530

    feat: Initialized DB Service

--- backend/src/db/db.service.ts
+++ backend/src/db/db.service.ts
@@ -1,23 +1,13 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient as PostgresClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class DbService extends PostgresClient implements OnModuleInit, OnModuleDestroy {
-  async onModuleInit() {
-    try {
-      await this.$connect();
-      console.log('Prisma Client Connected to DB');
-    } catch (error) {
-      console.error('Prisma Client Failed to connect to DB:', error);
-    }
-  }
-
-  async onModuleDestroy() {
-    try {
-      await this.$disconnect();
-      console.log('Prisma Client isconnected from DB');
-    } catch (error) {
-      console.log('Prisma Client Failed to disconnect from DB', error);
-    }
+export class DbService
+  extends PostgresClient
+{
+  constructor() {
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+    super({ adapter });
   }
 }
