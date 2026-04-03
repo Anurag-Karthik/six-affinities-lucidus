c98238ccaaf5cd8b1a85b7bc84fad7178f0b5733
Anurag-Karthik <anuragpolamarasetty@gmail.com>
2026-04-04 01:47:55 +0530

    fix: Removed Redundant User Controller

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { DbService } from '../db/db.service';
 
 @Module({
   providers: [UsersService, DbService],
-  controllers: [UsersController],
   exports: [UsersService]
 })
 export class UsersModule {}
