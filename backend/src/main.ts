b4db55149e99c7bc3c1523fe1239099cf43bee82
Anurag-Karthik <anuragpolamarasetty@gmail.com>
2026-04-04 01:44:07 +0530

    feat: Added CORS to let Backend accept requests from Frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,11 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
