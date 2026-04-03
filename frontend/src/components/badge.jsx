a12a4b5308e29b7331df7eecf7688833b087f7ef
Anurag-Karthik <anuragpolamarasetty@gmail.com>
2026-04-04 01:44:44 +0530

    feat: Implemented Blur Badge and Added Image Support to Badges

--- frontend/src/components/badge.jsx
+++ frontend/src/components/badge.jsx
@@ -1,12 +1,14 @@
-export default function Badge({ text, color }) {
+export default function Badge({ text, color, hasImage, imgSrc }) {
     const colors = {
         orange: 'bg-linear-to-b from-light-orange to-dark-orange text-white',
         'yellow': 'bg-linear-to-b from-light-yellow to-dark-yellow text-white',
-        'green': 'bg-linear-to-b from-light-green to-dark-green text-white'
+        'green': 'bg-linear-to-b from-light-green to-dark-green text-white',
+        'blur': 'bg-white/5 text-white'
     }
 
     return (
         <div className={`p-2 rounded-4xl leading-none text-sm ${colors[color]}`}>
+            {hasImage && <img src={imgSrc} alt="Badge" className="w-4 h-4 mr-1 inline-block" />}
             {text}
         </div>
     );
