cdebade1c4640d8ded01bc864b39271e7ed559a8
Anurag-Karthik <anuragpolamarasetty@gmail.com>
2026-04-03 11:13:14 +0530

    fix: Changed route

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Landing from "../pages/landing";
 import Assessment from "../pages/assessment";
-import Results from "../pages/result";
+import Result from "../pages/result";
 
 export default function AppRouter() {
   return (
@@ -10,7 +10,7 @@ export default function AppRouter() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/assessment" element={<Assessment />} />
-        <Route path="/results" element={<Results />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
     </BrowserRouter>
   );
