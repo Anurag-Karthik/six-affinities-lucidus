5493121b90aae2bc2b64cc3d1e76373c0cc0a596
Anurag-Karthik <anuragpolamarasetty@gmail.com>
2026-04-03 11:12:34 +0530

    fix: Changed header rendering from App Level to Component Level

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import AppRouter from './router/AppRouter'
-import Header from './components/header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='w-screen h-screen bg-linear-to-b from-purple to-purple-light'>
-      <Header />
+    <div className='w-screen min-h-screen bg-linear-to-b from-purple to-purple-light'>
       <AppRouter />
     </div>
   )
